0a20068992ecf2879dc7f9e72c56ae099e793eae
Jan Čermák <sairon@sairon.cz>
2024-05-14 11:48:53 +0200

    package/rpi-firmware: bump for testing build of latest master
    Signed-off-by: Jan Čermák <sairon@sairon.cz>

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_FIRMWARE_VERSION = 969420b4121b522ab33c5001074cc4c2547dafaf
+RPI_FIRMWARE_VERSION = 734829e3525e5baea62d1deedbe65eb60f4fb36b
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3-Clause
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
